3f36a2e41034fb5b2652aea3533544d64c45ed9d
Omar <omar0940@gmail.com>
2021-05-04 15:29:55 +0200

    Numbers Exchange

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ function registerUserValue(e) {
   userNumber = e.target.value;
 }
 function convert() {
+  let = currentConverterValue;
+  console.log(converter.value);
   let binary = "110100";
-  let digit = parseInt(binary, 2);
-  return digit;
+  switch (converter.value) {
+    case "binary":
+      currentConverterValue = parseInt(binary, 2);
+      break;
+    case "decimal":
+      currentConverterValue = userInput.toString(16);
+      break;
+    default:
+      currentConverterValue = 1;
+  }
+  result.innerHTML = `${currentConverterValue * parseInt(userNumber)}`;
 }
-console.log(convert);
+// console.log(convert);
 
 converter.addEventListener("change", convert);
 userInput.addEventListener("keyup", registerUserValue);
